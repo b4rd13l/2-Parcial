e55a200498a0be66dc50f19e984ac73d9a19b664
Alejandro Aspi <aspiguti45@gmail.com>
2025-05-03 12:15:51 -0400

    Agregar la segunda molécula

---
+++ app/componentes/molecula2.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const molecula2 = () => {
+  return (
+    <div className="molecula2">
+        <div className="m2NumeroCirculo">
+                <span>1</span>
+            </div>
+            
+            <div className="m2Agenda">
+                Agenda/Topic
+            </div>
+            
+            <div className="m2Saas">
+                SAAS-0000
+            </div>
+
+            <div className='m2Logo'>
+                <span className="logo">◆</span>
+            </div>
+    </div>
+  )
+}
+
+export default molecula2
