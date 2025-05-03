ffe7693216e345c4e01298a1f6669fa10c080f3d
Alejandro Aspi <aspiguti45@gmail.com>
2025-05-03 12:36:42 -0400

    Agregar cuarta molécula

---
+++ app/componentes/molecula4.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const molecula4 = () => {
+  return (
+    <div className="molecula4">
+        <div className="m4LastUpdate">Last Updated</div>
+        <div className="m4Fecha">3/8/2020</div>
+    </div>
+  )
+}
+
+export default molecula4
